4a3e562d5385e651f260a6cf3ff49e0dc4a190dd
BeardedTek <kenny@beardedtek.com>
2024-01-15 20:10:21 -0900

    Undo changes to install.sh Create install-dev.sh which includes the non-working checks

--- install.sh
+++ install.sh
@@ -9,87 +9,42 @@
 #
 DISK=/dev/sda
 
-# Some checks before we start
-PACKAGES=""
-niv
-NIV=$?
-sbctl
-SBCTL=$?
-if [[ ! "${NIV}" = "0" ]] || [[ ! "${SBCTL}" - "0" ]] || [ -z $DISK ]; then
-  if [[ ! "${NIV}" = "0" ]]; then
-    echo "niv     - MISSING"
-    PACKAGES="${PACKAGES} niv"
-  fi
-  if [[ ! "${SBCTL}" = "0" ]]; then
-    echo "sbctl   - MISSING"
-  fi
-  if [ -z $DISK ]; then
-    echo "\$DISK    - NOT SET"
-  fi
-  echo "Correct the preceeding errors before continuing!"
-else
-  echo "niv     - INSTALLED"
-  echo "sbctl   - INSTALLED"
-  echo "\$DISK   - OK"
-  echo -n "Continue with install?"
-  CONTINUE="x"
-  read CONTINUE
-  CONTINUE=$(echo "${CONTINUE}" | tr '[:upper:]' '[:lower:]')
-  if [[ ! "${CONTINUE}" = "y" ]] [[ ! "${CONTINUE}" = "yes" ]]; then
-    echo "Looks like you're not ready yet!  That's okay!  Try again when you're ready!"
-  fi
-fi
-
-
 if [[ $DISK = *nvme* ]]; then
   PART_MRKR="p"
 fi
 
-if [ -f ".boot_uuid" ] && [ -f ".root_uuid" ]; then
-  echo "Double check these details:"
-  echo "HOSTNAME       : ${HOSTNAME}"
-  echo "DISK           : ${DISK}"
-  echo "BOOT PARTITION : ${DISK}${PART_MRKR}1"
-  echo "ROOT PARTITION : ${DISK}${PART_MRKR}2"
-  
-  # Partition the drive with 550M boot partition and the rest btrfs
-  # printf "label: gpt\n,550M,U\n,,L\n" | sfdisk ${DISK}
-  
-  # Format the partitions
-  # /boot:
-  mkfs.fat -F 32 ${DISK}${PART_MRKR}1
-  
-  # /:
-  mkfs.btrfs -f ${DISK}${PART_MRKR}2
-  
-  
-  ROOT_UUID=$(ls -l /dev/disk/by-uuid | grep $(echo $DISK | sed 's/\/dev\///')p2 | awk '{print $9}')
-  BOOT_UUID=$(ls -l /dev/disk/by-uuid | grep $(echo $DISK | sed 's/\/dev\///')p1 | awk '{print $9}')
-  
-  echo "${BOOT_UUID}" > .boot_uuid
-  echo "${ROOT_UUID}" > .root_uuid
-  echo "BOOT_UUID : $BOOT_UUID"
-  echo "ROOT_UUID : $ROOT_UUID"
-  
+echo "Double check these details:"
+echo "HOSTNAME       : ${HOSTNAME}"
+echo "DISK           : ${DISK}"
+echo "BOOT PARTITION : ${DISK}${PART_MRKR}1"
+echo "ROOT PARTITION : ${DISK}${PART_MRKR}2"
 
-  echo -n "Continue ? (Y/n): "
-  read CONT
-  CONT=$(echo "${CONT}" | tr '[:upper:]' '[:lower:]')
-  if [[ ! "${CONT}" = "y" ]] || [[ ! "${CONT}" = "yes" ]] ||; then
-    echo "You must enter 'y' to contine."
-    echo "Exiting Installation."
-    exit
-  fi
-else
-  ROOT_UUID=$(cat .root_uuid)
-  ROOT_UUID_OK=$?
-  BOOT_UUID=$(cat .boot_UUID)
-  BOOT_UUID_OK=$?
-  if [[ ! "${ROOT_UUID}" = "0" ]] [[ ! "${BOOT_UUID}" = "0" ]]
-    echo "It *looks* like you've created root and boot partitions, but the data isn't there as expected."
-    echo "Exiting.  Fix it yourself!!!"
-    exit
-  fi
+# Partition the drive with 550M boot partition and the rest btrfs
+# printf "label: gpt\n,550M,U\n,,L\n" | sfdisk ${DISK}
+
+# Format the partitions
+# /boot:
+mkfs.fat -F 32 ${DISK}${PART_MRKR}1
+
+# /:
+mkfs.btrfs -f ${DISK}${PART_MRKR}2
+
+
+ROOT_UUID=$(ls -l /dev/disk/by-uuid | grep $(echo $DISK | sed 's/\/dev\///')p2 | awk '{print $9}')
+BOOT_UUID=$(ls -l /dev/disk/by-uuid | grep $(echo $DISK | sed 's/\/dev\///')p1 | awk '{print $9}')
+
+echo "${BOOT_UUID}" > .boot_uuid
+echo "${ROOT_UUID}" > .root_uuid
+echo "BOOT_UUID : $BOOT_UUID"
+echo "ROOT_UUID : $ROOT_UUID"
+echo -n "Continue ? (Y/n): "
+read CONT
+
+CONT=$(echo "${CONT}" | tr '[:upper:]' '[:lower:]')
+if [[ ! "${CONT}" = "y" ]] || [[ ! "${CONT}" = "yes" ]] ||; then
+  echo "You must enter 'y' to contine."
+  echo "Exiting Installation."
+  exit
 fi
 
 # Create subvolumes
@@ -126,7 +81,8 @@ cd $COMEBACK
 # Generate Secure Boot Keys
 echo -n "Generate Secure Boot Keys? (y/N)?: "
 read SEC_BOOT
-  SEC_BOOT=$(echo "${SEC_BOOT}" | tr '[:upper:]' '[:lower:]')
+SEC_BOOT=$(echo "${SEC_BOOT}" | tr '[:upper:]' '[:lower:]')
+
 if [[ "${SEC_BOOT}" = "y" ]] || [[ "${SEC_BOOT}" = "yes" ; then
   sbctl create-keys -d /mnt/etc/secureboot -e /mnt/etc/secureboot/keys
 else
